b50311322e3e5f0766d65af63322b7aedd83332e
Rakesh <rakesh22717@gmail.com>
2017-03-30 15:41:16 +0530

    Some more changes in Layout of navigation drawer

--- app/src/main/java/com/example/dell/smartkitchen/HomePage.java
+++ app/src/main/java/com/example/dell/smartkitchen/HomePage.java
@@ -12,14 +12,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class HomePage extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    TextView message;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        message=(TextView)findViewById(R.id.message1);
+        message.setSelected(true);
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
