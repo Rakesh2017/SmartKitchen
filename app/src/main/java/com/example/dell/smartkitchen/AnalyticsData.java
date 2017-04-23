f0d85ea65293da49b09e92b154ecbadf6e3b96e4
Rakesh <rakesh22717@gmail.com>
2017-04-23 23:56:00 +0530

    graph demo tested

--- app/src/main/java/com/example/dell/smartkitchen/AnalyticsData.java
+++ app/src/main/java/com/example/dell/smartkitchen/AnalyticsData.java
@@ -7,6 +7,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.jjoe64.graphview.GraphView;
+
+import android.graphics.Color;
+import android.util.Log;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.LegendRenderer;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,7 +35,28 @@ public class AnalyticsData extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_analytics_data, container, false);
+        View view = inflater.inflate(R.layout.fragment_analytics_data, container, false);
+        GraphView graph = (GraphView) view.findViewById(R.id.graph);
+        initGraph(graph);
+
+        return view;
+    }
+
+    public void initGraph(GraphView graph) {
+        // first series is a line
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        series.setDrawBackground(true);
+        series.setAnimated(true);
+        series.setDrawDataPoints(true);
+        series.setTitle("People");
+
+        graph.addSeries(series);
     }
 
 }
